668ca8ce6c258eb6fefd3e88ec84d0338ad6bdc6
Loic Thurre <loic.thurre@gmail.com>
2019-06-17 16:34:41 +0200

    added validations to the signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nom, presence: true, uniqueness: true
+  validates :prenom, presence: true, uniqueness: true
+  validates :adresse, presence: true
   geocoded_by :adresse
   after_validation :geocode, if: :will_save_change_to_adresse?
   has_many :paniers

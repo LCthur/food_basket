8643f60a478ce98e587bb0c91001c59e20f95da6
Loic Thurre <loic.thurre@gmail.com>
2019-06-17 16:32:51 +0200

    geocoder gem basic config done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  geocoded_by :adresse
+  after_validation :geocode, if: :will_save_change_to_adresse?
   has_many :paniers
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable

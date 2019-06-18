16e8bf2c4961f66357ba0460d5485e7983f2344b
Loic Thurre <loic.thurre@gmail.com>
2019-06-18 17:44:56 +0200

    commit to clean gst and add photo upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :paniers
-  root to: 'paniers#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

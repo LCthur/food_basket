1f298e31e70e69d796e6c0789e0f6abd9f131832
Loic Thurre <loic.thurre@gmail.com>
2019-06-25 13:37:07 +0200

    add adress to panier

--- app/controllers/paniers_controller.rb
+++ app/controllers/paniers_controller.rb
@@ -4,8 +4,6 @@ class PaniersController < ApplicationController
     @query = params[:query]
     @paniers_a_proximite = []
     User.near(@query, 10).each { |user| @paniers_a_proximite << user.paniers }
-    @paniers_a_proximite.map! { |panier| panier[0] }
-  end
 
   def show
     @panier = Panier.find(params[:id])

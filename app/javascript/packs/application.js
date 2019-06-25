4c076ada46ee27673a2d22a28b7b05258abddf72
Loic Thurre <loic.thurre@gmail.com>
2019-06-25 16:31:52 +0200

    implemented algolia autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
-import { initAutocomplete }; from '../plugins/init_autocomplete.js';
+import { initAutocomplete } from '../plugins/init_autocomplete.js';
 
 initMapbox();
 initAutocomplete();
--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -2,8 +2,11 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.getElementById('search-bar');
+  const navAddressInput = document.getElementById('search-navbar');
   if (addressInput) {
     places({ container: addressInput });
+  } else if (navAddressInput) {
+    places({ container: navAddressInput });
   }
 };
 
